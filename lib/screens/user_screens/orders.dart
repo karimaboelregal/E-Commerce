e7b7813d4ce97ff843f984655882b57fc153d4eb
JohnEmadElias <74542306+johnemadelias@users.noreply.github.com>
2021-12-10 21:47:23 +0200

    Orders first commit

--- lib/screens/user_screens/orders.dart
+++ lib/screens/user_screens/orders.dart
@@ -6,7 +6,35 @@ class Orders extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("hi"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 80),
+            Center(
+                child: Column(
+                  children: const [
+                    Icon(
+                      Icons.shopping_basket,
+                      size: 65.0,
+                      color: Color(0xff0088ff),
+                    ),
+                    Text(
+                      "Ecommerce",
+                      style: TextStyle(color: Color(0xff0088ff), fontSize: 35.0),
+                    ),
+                  ],
+                )),
+
+            WillPopScope(
+                onWillPop: () {
+                  print("hi");
+                  globals.currentTab.value = 3;
+                  return Future.value(false);
+                },
+                child: Text('')),
+          ],
+        ),
+      ),
     );
   }
 }

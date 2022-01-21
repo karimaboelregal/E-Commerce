1382209f51156f464832507d36d87bbe6f5ca4ce
johnemadelias <jmckarius1@gmail.com>
2022-01-21 18:01:11 +0200

    profile changes

---
+++ lib/screens/user_screens/ProfileScreen/profile_page.dart
@@ -0,0 +1,168 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'edit_description.dart';
+import 'edit_email.dart';
+import 'edit_image.dart';
+import 'edit_name.dart';
+import 'edit_phone.dart';
+import 'display_image_widget.dart';
+
+// This class handles the Page to dispaly the user's info on the "Edit Profile" Screen
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            toolbarHeight: 10,
+          ),
+          Center(
+              child: Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromRGBO(64, 105, 225, 1),
+                    ),
+                  ))),
+          InkWell(
+              onTap: () {
+                navigateSecondPage(EditImagePage());
+              },
+              child: DisplayImage(
+                imagePath: "sdf",
+                onPressed: () {},
+              )),
+          buildUserInfoDisplay("sdfad", 'Name', EditNameFormPage()),
+          buildUserInfoDisplay("sdfadf", 'Phone', EditPhoneFormPage()),
+          buildUserInfoDisplay("sdfadf", 'Email', EditEmailFormPage()),
+          Expanded(
+            child: buildAbout(),
+            flex: 4,
+          )
+        ],
+      ),
+    );
+  }
+
+  // Widget builds the display item with the proper formatting to display the user's info
+  Widget buildUserInfoDisplay(String getValue, String title, Widget editPage) =>
+      Padding(
+          padding: EdgeInsets.only(bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              Container(
+                  width: 350,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                    color: Colors.grey,
+                    width: 1,
+                  ))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                    Expanded(
+                        child: TextButton(
+                            onPressed: () {
+                              navigateSecondPage(editPage);
+                            },
+                            child: Text(
+                              "sdfasdf",
+                              style: TextStyle(fontSize: 16, height: 1.4, color: Colors.black),
+                            ))),
+                    Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Colors.grey,
+                      size: 40.0,
+                    )
+                  ]))
+            ],
+          ));
+
+  // Widget builds the About Me Section
+  Widget buildAbout() => Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Tell Us About Yourself',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 1),
+          Container(
+              width: 350,
+              height: 100,
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                color: Colors.grey,
+                width: 1,
+              ))),
+              child: Row(children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          navigateSecondPage(EditDescriptionFormPage());
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  "sdfasdfsdf",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    height: 1.4,
+                                  ),
+                                ))))),
+                Icon(
+                  Icons.keyboard_arrow_right,
+                  color: Colors.grey,
+                  size: 40.0,
+                )
+              ]))
+        ],
+      ));
+
+  // Refrshes the Page after updating user info.
+  FutureOr onGoBack(dynamic value) {
+    setState(() {});
+  }
+
+  // Handles navigation and prompts refresh.
+  void navigateSecondPage(Widget editForm) {
+    Route route = MaterialPageRoute(builder: (context) => editForm);
+    Navigator.push(context, route).then(onGoBack);
+  }
+}

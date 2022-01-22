9fc056dd06e479dbbc07bd09ea99532cd886b0ba
johnemadelias <jmckarius1@gmail.com>
2022-01-22 18:49:02 +0200

    made the button work in 3 pages now images to do

--- lib/screens/user_screens/HomeScreen/home_screen.dart
+++ lib/screens/user_screens/HomeScreen/home_screen.dart
@@ -24,7 +24,9 @@ class HomeScreen extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.account_circle_rounded),
             color: Color(0xff0088ff),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushNamed(context, "/profile");
+            },
           ),
           Container(
               padding: EdgeInsets.symmetric(vertical: 12.0),

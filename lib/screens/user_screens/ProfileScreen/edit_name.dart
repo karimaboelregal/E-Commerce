85681d46e6aa653e11ad37afeec7155643b3add0
johnemadelias <jmckarius1@gmail.com>
2022-01-22 17:45:06 +0200

    fixed all margin errors and overflows

--- lib/screens/user_screens/ProfileScreen/edit_name.dart
+++ lib/screens/user_screens/ProfileScreen/edit_name.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:string_validator/string_validator.dart';
 import 'appbar_widget.dart';
 
-// This class handles the Page to edit the Name Section of the User Profile.
+// This class handles the Page to edit the Phone Section of the User Profile.
 class EditNameFormPage extends StatefulWidget {
   const EditNameFormPage({Key? key}) : super(key: key);
-
   @override
   EditNameFormPageState createState() {
     return EditNameFormPageState();
@@ -14,108 +13,83 @@ class EditNameFormPage extends StatefulWidget {
 
 class EditNameFormPageState extends State<EditNameFormPage> {
   final _formKey = GlobalKey<FormState>();
-  final firstNameController = TextEditingController();
-  final secondNameController = TextEditingController();
+  final phoneController = TextEditingController();
 
   @override
   void dispose() {
-    firstNameController.dispose();
+    phoneController.dispose();
     super.dispose();
   }
 
-  void updateUserValue(String name) {
-    //user.name = name;
+  void updateUserValue(String phone) {
+    String formattedPhoneNumber = "(" +
+        phone.substring(0, 3) +
+        ") " +
+        phone.substring(3, 6) +
+        "-" +
+        phone.substring(6, phone.length);
+    //user.phone = formattedPhoneNumber;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: buildAppBar(context),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                  width: 330,
-                  child: const Text(
-                    "What's Your Name?",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                      width: 320,
+                      child: const Text(
+                        "What's your name?",
+                        style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                      )),
                   Padding(
-                      padding: EdgeInsets.fromLTRB(0, 40, 16, 0),
+                      padding: EdgeInsets.only(top: 40),
                       child: SizedBox(
                           height: 100,
-                          width: 150,
+                          width: 320,
                           child: TextFormField(
-                            // Handles Form Validation for First Name
+                            // Handles Form Validation
                             validator: (value) {
                               if (value == null || value.isEmpty) {
-                                return 'Please enter your first name';
-                              } else if (!isAlpha(value)) {
-                                return 'Only Letters Please';
+                                return 'Please enter your name';
                               }
                               return null;
                             },
-                            decoration:
-                                InputDecoration(labelText: 'First Name'),
-                            controller: firstNameController,
+                            controller: phoneController,
+                            decoration: const InputDecoration(
+                              labelText: 'Your Phone Number',
+                            ),
                           ))),
                   Padding(
-                      padding: EdgeInsets.fromLTRB(0, 40, 16, 0),
-                      child: SizedBox(
-                          height: 100,
-                          width: 150,
-                          child: TextFormField(
-                            // Handles Form Validation for Last Name
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your last name';
-                              } else if (!isAlpha(value)) {
-                                return 'Only Letters Please';
-                              }
-                              return null;
-                            },
-                            decoration:
-                                const InputDecoration(labelText: 'Last Name'),
-                            controller: secondNameController,
+                      padding: EdgeInsets.only(top: 150),
+                      child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: SizedBox(
+                            width: 320,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Validate returns true if the form is valid, or false otherwise.
+                                if (_formKey.currentState!.validate() &&
+                                    isNumeric(phoneController.text)) {
+                                  updateUserValue(phoneController.text);
+                                  Navigator.pop(context);
+                                }
+                              },
+                              child: const Text(
+                                'Update',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
                           )))
-                ],
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: 150),
-                  child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SizedBox(
-                        width: 330,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Validate returns true if the form is valid, or false otherwise.
-                            if (_formKey.currentState!.validate() &&
-                                isAlpha(firstNameController.text +
-                                    secondNameController.text)) {
-                              updateUserValue(firstNameController.text +
-                                  " " +
-                                  secondNameController.text);
-                              Navigator.pop(context);
-                            }
-                          },
-                          child: const Text(
-                            'Update',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                        ),
-                      )))
-            ],
+                ]),
           ),
         ));
   }

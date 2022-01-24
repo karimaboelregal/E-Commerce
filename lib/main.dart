a7b9a763613488629b9ade75a9c96b56e545180b
johnemadelias <jmckarius1@gmail.com>
2022-01-24 03:01:35 +0200

    notifications first release

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,16 @@ import 'package:e_commerce1/services/profile.dart';
 import 'package:e_commerce1/size_config.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:e_commerce1/providers/cart_provider.dart';
 
 import 'screens/user_screens/cart/cart_screen.dart';
 import 'screens/user_screens/notifications//notifications_screen.dart';
+
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +23,45 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final FBAPP = Firebase.initializeApp();
+
+  // AndroidNotificationChannel channel = AndroidNotificationChannel(
+  //   'high_importance_channel', // id
+  //   'High Importance Notifications', // title
+  //   description: 'This channel is used for important notifications.', // description
+  //   importance: Importance.max,
+  // );
+  // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  // FlutterLocalNotificationsPlugin();
+  //
+  // Future asyncFunction() async {
+  //   await flutterLocalNotificationsPlugin
+  //       .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+  //       ?.createNotificationChannel(channel);
+  //   print("here2");
+  //   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //     RemoteNotification? notification = message.notification;
+  //     AndroidNotification? android = message.notification?.android;
+  //
+  //     // If `onMessage` is triggered with a notification, construct our own
+  //     // local notification to show to users using the created channel.
+  //     if (notification != null && android != null) {
+  //       flutterLocalNotificationsPlugin.show(
+  //           notification.hashCode,
+  //           notification.title,
+  //           notification.body,
+  //           NotificationDetails(
+  //             android: AndroidNotificationDetails(
+  //               channel.id,
+  //               channel.name,
+  //               channelDescription: channel.description,
+  //               icon: android.smallIcon,
+  //               // other properties...
+  //             ),
+  //           ));
+  //     }
+  //   });
+  //   return FBAPP;
+  // }
 
   @override
   Widget build(BuildContext context) {
